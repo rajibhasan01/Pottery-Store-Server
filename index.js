847b20d3a61d23103288d4fedfc3829da952cedf
unknown <hasan.rajib1996@gmail.com>
2021-12-03 19:10:38 +0600

    updated sslcommerz

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const { parse } = require('dotenv');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 
+// stripe import
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+// sslcommerz import
+const SSLCommerzPayment = require('sslcommerz');
 
 
 const serviceAccount = require('./potteryshop3-firebase-adminsdk.json');
@@ -272,7 +276,77 @@ async function run() {
             res.json({
                 clientSecret: paymentIntent.client_secret
             });
+        });
+
+        // payment initialization api
+        app.post('/init', (req, res) => {
+            console.log(req.body);
+            const data = {
+                total_amount: req.body.total_amount,
+                currency: 'BDT',
+                tran_id: 'REF123',
+                success_url: 'https://immense-mountain-96317.herokuapp.com/success',
+                fail_url: 'https://immense-mountain-96317.herokuapp.com/fail',
+                cancel_url: 'https://immense-mountain-96317.herokuapp.com/cancel',
+                ipn_url: 'https://immense-mountain-96317.herokuapp.com/ipn',
+                shipping_method: 'Courier',
+                product_name: req.body.product_name,
+                product_category: 'Electronic',
+                product_profile: 'general',
+                cus_name: req.body.cus_name,
+                cus_email: req.body.cus_email,
+                cus_add1: 'Dhaka',
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: '01711111111',
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+                multi_card_name: 'mastercard',
+                value_a: 'ref001_A',
+                value_b: 'ref002_B',
+                value_c: 'ref003_C',
+                value_d: 'ref004_D'
+            };
+
+            const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD, false) //true for live default false for sandbox
+            sslcommer.init(data).then(data => {
+                //process the response that got from sslcommerz 
+                //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+                // console.log(data);
+                if (data.GatewayPageURL) {
+                    res.json(data.GatewayPageURL)
+                }
+                else {
+                    return res.status(400).json({
+                        message: 'payment session failed'
+                    })
+                }
+            });
         })
+
+        app.post('/success', async (req, res) => {
+            // console.log(req.body);
+            res.status(200).redirect('https://potteryshop3.web.app/dashboard/success');
+        })
+        app.post('/fail', async (req, res) => {
+            // console.log(req.body);
+            res.status(400).redirect('https://potteryshop3.web.app/dashboard/failed');
+        })
+        app.post('/cancel', async (req, res) => {
+            // console.log(req.body);
+            res.status(200).json(req.body);
+        })
+
+
 
 
 
@@ -282,6 +356,8 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
